5e1969c07ac1aa03960d5a93735dd66c8cbc88e1
Saúl García Céspedes <saul@dixwe.com>
2023-08-24 17:58:42 +0200

    feat: minor animations

--- app/src/app/components/RoadmapSwiper.tsx
+++ app/src/app/components/RoadmapSwiper.tsx
@@ -99,12 +99,12 @@ export default function RoadmapSwiper() {
         ))}
       </Swiper>
       <div className='absolute bottom-4 right-16 z-10 flex justify-center gap-5'>
-        <button className='p-3 bg-[#C4C3BF] text-white rounded-2xl focus-visible:outline-stone-700' onClick={()=> swiperRef.current.slidePrev()}>
+        <button className='p-3 bg-[#C4C3BF] transition-all hover:bg-[#8d8c8a] text-white rounded-2xl focus-visible:outline-stone-700 ' onClick={()=> swiperRef.current.slidePrev()}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
           </svg>
         </button>
-        <button className='p-3 bg-[#C4C3BF] text-white rounded-2xl focus-visible:outline-stone-700' onClick={()=> swiperRef.current.slideNext()}>
+        <button className='p-3 bg-[#C4C3BF] transition-all hover:bg-[#8d8c8a] text-white rounded-2xl focus-visible:outline-stone-700' onClick={()=> swiperRef.current.slideNext()}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
           </svg>
