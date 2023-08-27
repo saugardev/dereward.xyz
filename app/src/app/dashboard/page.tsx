22916a47c5d16fbe6404ba81b2c25e4f2eda127b
Saúl García Céspedes <saul@dixwe.com>
2023-08-27 21:38:21 +0200

    feat: dasboard elements

--- app/src/app/dashboard/page.tsx
+++ app/src/app/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import { ArrowPathIcon, CalendarDaysIcon, PlusIcon } from "@heroicons/react/24/outline";
 import Logo from "../components/Logo";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function DashboardPage() {
   return (
-    <div className="text-stone-700 flex flex-col justify-between mx-auto rounded-[3rem] w-full max-w-7xl min-h-[40rem] px-6 lg:flex lg:px-10 pt-16 pb-8 bg-[#F5F4EF] my-10 mb-20">
-      <div className="w-full flex pb-9">
+    <div className="text-stone-700 flex flex-col items-center justify-between mx-auto rounded-[3rem] w-full max-w-7xl min-h-[40rem] px-6 lg:flex lg:px-10 pt-16 pb-8 bg-[#F5F4EF] my-10 mb-20">
+      <div className="w-full flex flex-col pb-9">
         <div className="w-full flex items-center">
           <h1 className="text-4xl sm:text-5xl">Dashboard</h1>
           <div className="flex ml-auto gap-3 items-center">
@@ -16,6 +17,41 @@ export default function DashboardPage() {
               <PlusIcon className="h-7 w-7"/>
             </button>
           </div>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="flex flex-col">
+          <h2 className="mt-10 text-2xl leading-8 text-stone-500 mx-auto">
+            Phala Hacker!
+          </h2>
+          <Image 
+            src='/card.png'
+            alt='card image'
+            height={400}
+            width={400}
+            className="w-128 h-128 opacity-80"/>
+        </div>
+        <div className="flex flex-col">
+          <h2 className="mt-10 text-2xl leading-8 text-stone-500 mx-auto">
+            Goal Number 2!
+          </h2>
+          <Image 
+            src='/card.png'
+            alt='card image'
+            height={400}
+            width={400}
+            className="w-128 h-128 opacity-80"/>
+        </div>
+        <div className="flex flex-col">
+          <h2 className="mt-10 text-2xl leading-8 text-stone-500 mx-auto">
+            Goal Number 3!
+          </h2>
+          <Image 
+            src='/card.png'
+            alt='card image'
+            height={400}
+            width={400}
+            className="w-128 h-128 opacity-80"/>
         </div>
       </div>
       {/* <div className="flex justify-between mx-auto mt-10 w-full max-w-7xl lg:flex">
